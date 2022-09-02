0e9501a19f7f382108f6f8d7ae958db81cf999d3
jennifer <jennifermariehoskins@gmail.com>
2022-09-02 17:53:51 -0400

    Getting rid of token

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(require('./config/checkToken'));
+
 
 const port = process.env.PORT ||  3001;
 
