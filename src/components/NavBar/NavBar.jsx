2d6c097f2f697887fb8c8d3a760475ae73ebce85
jennifer <jennifermariehoskins@gmail.com>
2022-09-05 23:48:07 -0400

    testing some things

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,9 +10,9 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to='/orders'>Order History</Link>
+      <Link to='/orders'>Your Profile</Link>
       &nbsp; | &nbsp;
-      <Link to='/orders/new'>New Order</Link>
+      <Link to='/orders/new'>Find a book</Link>
       &nbsp; | &nbsp;
       Welcome, {user.name}
       &nbsp; | &nbsp;
