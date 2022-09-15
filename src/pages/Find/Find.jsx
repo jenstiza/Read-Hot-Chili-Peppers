7b1c6de3599e11a139e0b3bfa7bf7a7782edb935
jennifer <jennifermariehoskins@gmail.com>
2022-09-15 00:59:46 -0400

    added instructions on the search page based on feedback

--- src/pages/Find/Find.jsx
+++ src/pages/Find/Find.jsx
@@ -12,16 +12,18 @@ export default function Find({getGoogleBooks, bookResults}) {
   return (
     <>
       <div className="find">
-        <div className="find-header"><h1>Find your next great read!</h1></div>
+        <div className="find-header"><h1>&nbsp;Find your next great read!</h1></div>
         <br /><br />
         <br /><br />
         <br /><br />
         <form onSubmit={(evt) => getGoogleBooks(evt,searchWord)}>          
-          <input type="text" placeholder="Enter your book name" className="search" value={searchWord} onChange={handleChange} />
+          <input type="text" placeholder="Enter your book name and then click the magnifying glass" className="search" value={searchWord} onChange={handleChange} />
           <div className='magnify'>
           <button type="submit"><i className="fas fa-search"></i></button>
           </div>
         </form>
+        <br/>
+        <h1 className='instructions'>&nbsp;&#8595;&nbsp;After Hitting the magnifying glass, scroll down for results!&nbsp;&#8595;&nbsp;</h1>
       </div>
       <SearchResults bookResults={bookResults}/>
     </>
